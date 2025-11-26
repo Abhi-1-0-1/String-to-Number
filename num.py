85dca8285a5bf78d8a612a383c80e2708ccff572
Abhi-1-0-1 <abhinavmparasher@gmail.com>
2025-11-26 13:38:15 +0400

    Update num.py

--- num.py
+++ num.py
@@ -1,14 +1,13 @@
-#initial dictionary of number words to their numeric values
-
+# Initial dictionary of number words to their numeric values
 values = {
     "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4,
     "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
     "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14,
-    "fifteen": 15, "sixteen": 16, "seventeen": 17, "eighteen": 18, "nineteen": 19
-    ,
+    "fifteen": 15, "sixteen": 16, "seventeen": 17, "eighteen": 18, "nineteen": 19,
+    
     "twenty": 20, "thirty": 30, "forty": 40, "fifty": 50,
-    "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90
-    ,
+    "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90,
+    
     "hundred": 100,
     "thousand": 1000,
     "million": 1000000,
@@ -19,17 +18,15 @@ values = {
 #Take input from user and clean it
 n = input("String: ").lower().strip()
 words = n.split()
-
-for i in words:
-    if i == "and":
-        words.remove(i)
+words = [word for word in words if word != "and"]
 
 # Split the words by large number multiples    
 split_by_multiples = []
 for i in words:
-    if i in ["thousand", "million", "billion", "trillion"]:
+    if i in list(values.keys())[list(values.keys()).index("thousand"):]:
         split_by_multiples.append(words[:words.index(i)+1])
         words = words[words.index(i)+1:]
+
 split_by_multiples.append(words)
 
 # Calculate the numeric value for each segment
@@ -37,17 +34,11 @@ for index, segment in enumerate(split_by_multiples):
     temp = 0
     for word in segment:
         value = values[word]
-        if word == "hundred":
-            temp *= value
-        elif word in ["thousand", "million", "billion", "trillion"]:
+        if word in list(values.keys())[list(values.keys()).index("hundred"):]:
             temp *= value
         else:
             temp += value
-        #print(word, value, temp)
-    
-    #print("Segment total:", temp)
     split_by_multiples[index] = temp
-
 number = sum(split_by_multiples)
 
 # Add commas to the number for readability
